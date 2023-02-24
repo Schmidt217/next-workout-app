0ddadcb08a3ea57f30b71b4f2f27927792f4cd90
Mike Schmidt <schmidt217@gmail.com>
2023-02-24 11:35:03 -0700

    setting initial auth to null

--- firebase.js
+++ firebase.js
@@ -29,7 +29,7 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
-export const auth = getAuth();
+export const auth = getAuth() || null;
 const googleProvider = new GoogleAuthProvider();
 const facebookProvider = new FacebookAuthProvider();
 
